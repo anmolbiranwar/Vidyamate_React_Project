3524bdb0645e4258d8a9c5ff24f049d95066c863
anmolbiranwar <83487792+anmolbiranwar@users.noreply.github.com>
2023-11-27 22:38:56 -0800

    Update Login.js

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -2,19 +2,19 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div className='d-flex justify-content-center align-items-center vh-100 loginPage'>
-        <div className='p-3 rounded border loginForm'>
+    <div className='d-flex justify-content-center align-items-center vh-100 loginPage bg-primary'>
+        <div className='p-3 rounded border loginForm bg-white'>
         <h2>LOGIN TO YOUR ACCOUNT</h2>
         <form>
-            <div>
-                <label htmlFor='email'><strong>Email:</strong></label>
-                <input type='email' name='email' autoComplete='off' placeholder='Enter Email' className='form-control rounded'/>
+            <div className='input-group '>
+                <input type='email' name='email' autoComplete='off' placeholder='Enter Email' className='form-control rounded'/><span className="input-group-text bi bi-person" id="inputGroupPrepend3"></span>
             </div>
-            <div>
-                <label htmlFor='password'><strong>Password:</strong></label>
-                <input type='password' name='password' autoComplete='off' placeholder='Enter Password' className='form-control rounded'/>
+            <br/>
+            <div className='input-group'>
+                <input type='password' name='password' autoComplete='off' placeholder='Enter Password' className='form-control rounded'/><i className="input-group-text bi bi-eye-slash" id="togglePassword"></i>
             </div>
-            <button className='btn btn-success w-100 rounded-0'>Submit</button>
+            <br/>
+            <button className='btn btn-success w-100 rounded-0 bg-primary'>Sign in</button>
         </form> 
         </div>
     </div>
