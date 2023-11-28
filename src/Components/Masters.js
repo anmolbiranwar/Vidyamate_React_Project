3237d3320b52fbdbc2ee8fce36988e914f5a4e54
anmolbiranwar <83487792+anmolbiranwar@users.noreply.github.com>
2023-11-28 11:05:09 -0800

    Create Masters.js

---
+++ src/Components/Masters.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Dashboard from './Dashboard'
+
+const Masters = () => {
+  return (
+    <>
+    <Dashboard />
+    <div>Masters</div>
+    </>
+  )
+}
+
+export default Masters
