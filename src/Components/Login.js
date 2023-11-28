7967bb977c27a5e3d31d116191f0f6cf072c3cf5
anmolbiranwar <83487792+anmolbiranwar@users.noreply.github.com>
2023-11-27 22:59:46 -0800

    Update Login.js

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -7,11 +7,11 @@ const Login = () => {
         <h2>LOGIN TO YOUR ACCOUNT</h2>
         <form>
             <div className='input-group '>
-                <input type='email' name='email' autoComplete='off' placeholder='Enter Email' className='form-control rounded'/><span className="input-group-text bi bi-person" id="inputGroupPrepend3"></span>
+                <input type='email' name='email' autoComplete='off' placeholder='Enter Email' className='form-control rounded'/><span className="input-group-text bi bi-person-fill" id="inputGroupPrepend3"></span>
             </div>
             <br/>
             <div className='input-group'>
-                <input type='password' name='password' autoComplete='off' placeholder='Enter Password' className='form-control rounded'/><i className="input-group-text bi bi-eye-slash" id="togglePassword"></i>
+                <input type='password' name='password' autoComplete='off' placeholder='Enter Password' className='form-control rounded'/><i className="input-group-text bi bi-eye-slash-fill" id="togglePassword"></i>
             </div>
             <br/>
             <button className='btn btn-success w-100 rounded-0 bg-primary'>Sign in</button>
