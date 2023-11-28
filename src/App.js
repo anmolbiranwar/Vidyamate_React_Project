83a007be9fdb81ef2f3db9dddc5a2578baa5650e
anmolbiranwar <83487792+anmolbiranwar@users.noreply.github.com>
2023-11-28 11:05:18 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,35 @@ import  '../node_modules/bootstrap/dist/css/bootstrap.css';
 import  '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
 
 import Login from './Components/Login';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Dashboard from './Components/Dashboard';
+import Masters from './Components/Masters';
+import Organization from './Components/Organization';
+import AcademicSession from './Components/AcademicSession';
+import SchoolRegistration from './Components/SchoolRegistration';
+import Users from './Components/Users';
+
+
 
 function App() {
   return (
-   <>
-   <Login/>
-   </>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Login/>}/>
+      <Route path='/dashboard' element={<Dashboard/>} />
+      <Route path='/masters' element={<Masters/>} />
+      <Route path='/organization' element={<Organization/>} />
+      <Route path='/academicsession' element={<AcademicSession/>} />
+      <Route path='/schoolregistration' element={<SchoolRegistration/>} />
+      <Route path='/users' element={<Users/>} />
+
+
+
+    </Routes>
+    </BrowserRouter>
+   
+   
+  
   );
 }
 
