176d3dc3d3e285b5c57ea6371250982a47eec5b3
anmolbiranwar <83487792+anmolbiranwar@users.noreply.github.com>
2023-11-28 11:05:00 -0800

    Create SchoolRegistration.js

---
+++ src/Components/SchoolRegistration.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Dashboard from './Dashboard'
+
+const SchoolRegistration = () => {
+  return (
+   <>
+    <Dashboard/>
+    <div>SchoolRegistration</div>
+   </>
+  )
+}
+
+export default SchoolRegistration
