e89182ab1b6c2cf62fa1323641311d47ffdf1526
anmolbiranwar <83487792+anmolbiranwar@users.noreply.github.com>
2023-11-29 12:15:29 -0800

    Update SchoolRegistration.js

--- src/Components/SchoolRegistration.js
+++ src/Components/SchoolRegistration.js
@@ -1,11 +1,28 @@
 import React from 'react'
+import { Link, Outlet, useLocation } from 'react-router-dom'
 
 
 const SchoolRegistration = () => {
+  const location=useLocation();
   return (
-   <>
-    <div>SchoolRegistration</div>
-   </>
+    <>
+    <div className='container-fluid'>
+    <div className='d-flex justify-content-between mt-3'>
+      <div className='fw-bold'>School Registration</div>
+      <div>
+        <Link to='/dashboard/schoolregistration/schoollist' className={`text-decoration-none m-2 ${location.pathname === '/dashboard/schoolregistration/schoollist' ? 'text-blue' : 'text-black'}`}>
+          <span>School List</span>
+        </Link>
+        <Link to='/dashboard/schoolregistration/addschool' className={`text-decoration-none m-3 ${location.pathname === '/dashboard/schoolregistration/addschool' ? 'text-blue' : 'text-black'}`}>
+          <span>Add School</span>
+        </Link>
+      </div>
+    </div>
+    <Outlet />
+    </div>
+    
+   
+      </>
   )
 }
 
