a2ec4801df441aa5ce9ba763a653f6a436b999df
anmolbiranwar <83487792+anmolbiranwar@users.noreply.github.com>
2023-11-29 11:25:56 -0800

    Update SchoolRegistration.js

--- src/Components/SchoolRegistration.js
+++ src/Components/SchoolRegistration.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import Dashboard from './Dashboard'
+
 
 const SchoolRegistration = () => {
   return (
    <>
-    <Dashboard/>
     <div>SchoolRegistration</div>
    </>
   )
