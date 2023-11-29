b2768d404a1ce9e3661e556b9f96ad6bd3a8ecf7
anmolbiranwar <83487792+anmolbiranwar@users.noreply.github.com>
2023-11-29 12:15:14 -0800

    Create AddSchool.js

---
+++ src/Components/AddSchool.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import SchoolRegistration from './SchoolRegistration'
+
+const AddSchool = () => {
+  return (
+   <>
+   <SchoolRegistration />
+    <div>AddSchool</div>
+   </>
+  )
+}
+
+export default AddSchool
