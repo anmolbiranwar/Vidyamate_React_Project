4e6edd1964c2aa30fe7a0ebc4c2e0c66aef54067
anmolbiranwar <83487792+anmolbiranwar@users.noreply.github.com>
2023-12-02 19:59:40 -0800

    Create BasicInfo.js

---
+++ src/Components/BasicInfo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import SchoolRegistration from './SchoolRegistration'
+import AddSchool from './AddSchool'
+
+const BasicInfo = () => {
+  return (
+   <>
+    <div><AddSchool/></div>
+   </>
+    )
+}
+
+export default BasicInfo
