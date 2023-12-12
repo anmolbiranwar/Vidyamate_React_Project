8fbeaaab004bf7392cf4476744e26b898ddde1f6
anmolbiranwar <83487792+anmolbiranwar@users.noreply.github.com>
2023-12-11 22:13:46 -0800

    Update ClassesSection.js

--- src/Components/ClassesSection.js
+++ src/Components/ClassesSection.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import SchoolRegistration from './SchoolRegistration'
+import RegistrationMenu from './RegistrationMenu'
 
 const ClassesSection = () => {
   return (
-    <div>ClassesSection</div>
+    <>
+    <SchoolRegistration />
+    <div className='d-flex'>
+      <RegistrationMenu/>
+      <div className='border rounded m-2 w-100'>
+      <h1>class and section</h1>
+      </div>
+    </div>
+    </>
+   
   )
 }
 
