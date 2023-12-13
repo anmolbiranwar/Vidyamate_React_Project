82c5c5d58efe805a7f8320a73f9a42eadb0c1aab
anmolbiranwar <83487792+anmolbiranwar@users.noreply.github.com>
2023-12-12 22:14:33 -0800

    Update FeesStructure.js

--- src/Components/FeesStructure.js
+++ src/Components/FeesStructure.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import SchoolRegistration from './SchoolRegistration'
+import RegistrationMenu from './RegistrationMenu'
 
 const FeesStructure = () => {
   return (
-    <div>FeesStructure</div>
+    <>
+    <SchoolRegistration />
+    <div className='d-flex'>
+      <RegistrationMenu />
+      <div className='border rounded m-2 w-100'>
+      <h1>Fees Structure</h1>
+      </div>
+    </div>
+    </>
   )
 }
 
